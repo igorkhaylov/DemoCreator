d7508d2413e34c023430fa54a2ee4cfbc00eb02c
igorkhaylov archlinux <igorkhaylov@yandex.com>
2021-06-23 12:53:55 +0500

    added pagination

--- main/urls.py
+++ main/urls.py
@@ -4,6 +4,7 @@ from main import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    # path('', views.Index.as_view(), name="index"),
     path('typography', views.typography, name="typography"),
     path('about', views.about_us, name="about"),
     path('contact', views.contact_us, name="contact"),
--- main/views.py
+++ main/views.py
@@ -1,11 +1,37 @@
 from django.shortcuts import render
 from .models import News, NewsImages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView, DetailView
 from django.http import HttpResponse
 
 
+# class Index(ListView):
+#     model = News
+#     template_name = "main/index.html"
+#     context_objects_name = 'news'
+#     paginate_by = 2
+#
+#     def get_context_data(self, *, object_list=None, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['title'] = "Classic Blog Design"
+#         return context
 def index(request):
     news = News.objects.filter(is_published=True)
-    return render(request, "main/index.html", context={"news": news})
+    page = request.GET.get('page', 1)
+
+    paginator = Paginator(news, 6)
+    try:
+        page_obj = paginator.page(page)
+    except PageNotAnInteger:
+        page_obj = paginator.page(1)
+    except EmptyPage:
+        page_obj = paginator.page(paginator.num_pages)
+    # paginator = Paginator(news, 2) # Show 25 contacts per page.
+    # page_number = request.GET.get('page')
+    # page_obj = paginator.get_page(page_number)
+    return render(request, "main/index.html", context={"news": news,
+                                                       "page_obj": page_obj,
+                                                       })
 
 
 def post_detail(request, id):
